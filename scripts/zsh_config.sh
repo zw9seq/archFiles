4feb025dcba501176cdf33d1a476043b00410188
zw9seq <zw9seq@gmail.com>
2025-08-28 04:56:32 -0400

    Update zsh_config.sh

--- scripts/zsh_config.sh
+++ scripts/zsh_config.sh
@@ -6,11 +6,11 @@
 
 set -e
 
-echo -e "/n>> Configuring Zsh.../n"
+echo -e "\n>> Configuring Zsh...\n"
 
 # Install zsh
 if ! command -v zsh &>/dev/null; then
-    echo -e "/n>> Installing zsh.../n"
+    echo -e "\n>> Installing zsh...\n"
     if command -v apt-get &>/dev/null; then
         sudo apt-get install -y zsh
     elif command -v pacman &> /dev/null; then
@@ -27,7 +27,7 @@ ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
 # Install oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo ">> Installing oh-my-zsh..."
+    echo e "\n>> Installing oh-my-zsh...\n"
     RUNZSH=no CHSH=no KEEP_ZSHRC=yes sh -c \
         "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
@@ -36,25 +36,25 @@ fi
 
 # Install Powerlevel10K
 if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
-    echo ">> Installing Powerlevel10k theme..."
+    echo -e "\n>> Installing Powerlevel10k theme...\n"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 fi
 
 # zsh-autosuggestions
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-    echo ">> Installing zsh-autosuggestions..."
+    echo -e "\n>> Installing zsh-autosuggestions...\n"
     git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 fi
 
 # zsh-syntax-highlighting
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-    echo ">> Installing zsh-syntax-highlighting..."
+    echo -e "\n>> Installing zsh-syntax-highlighting...\n"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
 
 # Install fzf
 if ! command -v fzf &> /dev/null; then
-    echo ">> Installing fzf..."
+    echo -e "\n>> Installing fzf...\n"
     if command -v apt-get &> /dev/null; then
         sudo apt-get install -y fzf
     elif command -v pacman &> /dev/null; then
@@ -66,8 +66,8 @@ fi
 
 # Stablish zsh as the default shell
 if [ "$SHELL" != "$(which zsh)" ]; then
-    echo ">> Changing default shell to zsh..."
+    echo -e "\n>> Changing default shell to zsh...\n"
     chsh -s "$(which zsh)"
 fi
 
-echo ">> Zsh setup completed!"
+echo -e "\n>> Zsh setup completed!\n"
